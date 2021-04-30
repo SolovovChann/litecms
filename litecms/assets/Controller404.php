9c9abb376b6a3b94c4256ed3c69fc39762f85211
Anastasia-AdminXakc <hundsamurai@gmail.com>
2021-04-30 13:23:35 +0500

    Add 404 page controller

---
+++ litecms/assets/Controller404.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Litecms\Assets;
+
+use Litecms\Core\Models\Controller;
+use Litecms\Core\Models\View;
+
+class Controller404 extends Controller
+{
+    public function default ()
+    {
+        $message = $_GET['msg'];
+        echo View::render ('404.php', [
+            'title' => 'Something gone wrong',
+            'message' => $message
+        ]);
+    }
+}
--- litecms/config/routes.php
+++ litecms/config/routes.php
@@ -5,4 +5,6 @@ namespace Litecms\Config;
 use Litecms\Core\Models\Router;
 
 Router::addindex ('Litecms\Apps\Home\HomeController');
+Router::add ('404', 'Litecms\Assets\Controller404');
 Router::add ('home', 'Litecms\Apps\Home\HomeController');
+Router::add ('admin', 'Litecms\Apps\Admin\AdminController');
