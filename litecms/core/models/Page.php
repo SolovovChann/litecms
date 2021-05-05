a7749a5851cda6bb1e3ff34991c505e9f1fc07e5
Anastasia-AdminXakc <hundsamurai@gmail.com>
2021-05-05 13:30:11 +0500

    Add new basic class Page

---
+++ litecms/core/models/Page.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Litecms\Core\Models;
+
+use Litecms\Core\Models\Model;
+
+class Page extends Model
+{
+    public static $table = 'pages';
+    public static $verboseName = 'Страница';
+    public static $verboseNamePlural = 'Страницы';
+
+    public $id;
+    public $url;
+    public $title;
+    public $description; 
+}
