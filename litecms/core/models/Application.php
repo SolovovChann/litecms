3daaa8639de867ec22d2c1c5ec1f8eddf434235a
Anastasia-AdminXakc <hundsamurai@gmail.com>
2021-05-07 13:55:57 +0500

    Add default timezone and start sesstion at app start

--- litecms/core/models/Application.php
+++ litecms/core/models/Application.php
@@ -3,7 +3,9 @@
 namespace Litecms\Core\Models;
 
 use Litecms\Core\Models\Router;
+use Litecms\Core\Models\User;
 use const Litecms\Config\Project\Name;
+use const Litecms\Config\Project\TimeZone;
 
 class Application
 {
@@ -15,8 +17,11 @@ class Application
      * @return void
      */
     public function start () {
-        Router::start ();
+        date_default_timezone_set(TimeZone);
+        session_start ();
         
+        User::checkToken ();
+        Router::start ();
     }
 
     /**
@@ -25,4 +30,11 @@ class Application
      * @param string $name – name of application. Will be used in namespace
      */
     public function createapp (string $name) {}
+
+    /**
+     * Walkthrough   all installed applications and create it's tables in db
+     * 
+     * @return void
+     */
+    public function migrate () {}
 }
