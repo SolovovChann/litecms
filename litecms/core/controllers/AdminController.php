5aa98efbce6b5a9ea774bec838503076a4587d2b
SolovovChann <solnik2001@gmail.com>
2021-05-10 23:58:35 +0500

    Remove redirect

--- litecms/core/controllers/AdminController.php
+++ litecms/core/controllers/AdminController.php
@@ -11,8 +11,6 @@ class AdminController extends Controller
 {
     public function default ()
     {
-        return Router::redirect ('HomeController:test:arg1:arg2:argN');
-        
         echo View::render ('admin.php', [
             'title' => 'Admin',
             'apps' => Applications,
