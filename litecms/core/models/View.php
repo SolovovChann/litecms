4f8ff2246bdd2947162fe6639f5c5a9d8ea13025
SolovovChann <solnik2001@gmail.com>
2021-05-28 02:00:46 +0500

    Move get_template to special function, add exetnd

--- litecms/core/models/View.php
+++ litecms/core/models/View.php
@@ -18,24 +18,49 @@ class View
      * @return string
      */
     public static function render (string $template, array $context = []) {
-        // Search all folders, set in config file
+        $file = View::get_template ($template);
+        
+        if (!$file) {
+            echo "Can't file template '$template'. Check for DIRS in litecms/config/project.php file.";
+            return;
+        }
+
+        // Provide access to context within a template 
+        extract ($context, EXTR_SKIP);
+
+        // Start buffering
+        ob_start ();
+        include $file;
+        $render = ob_get_contents ();
+        ob_end_clean ();
+
+        return $render;
+    }
+
+
+    /**
+     * Extend existing template with new template 
+     */
+    public static function extend (string $base, string $template, array $context = [])
+    {
+        $extender = View::render ($template, $context);
+        $context['MAIN'] = $extender;
+
+        return View::render ($base, $context);
+    }
+
+
+    /**
+     * 
+     */
+    public static function get_template (string $template)
+    {
         foreach (Dirs['templates'] as $folder) {
             $file = Filesystem::path ($folder, $template);
 
-            if (file_exists ($file)) {               
-                // Context variables is avalible as regular variables
-                extract ($context, EXTR_SKIP);
-        
-                // Start buffering
-                ob_start ();
-                include $file;
-                $render = ob_get_contents ();
-                ob_end_clean ();
-        
-                return $render;
+            if (file_exists ($file)) {
+                return $file;
             }
         }
-
-        Router::throw404 ("Template '$template' is not exists");
     }
 }
