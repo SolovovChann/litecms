dc355dd5b337292e72d7edb6cdc6e11499f8f6e3
SolovovChann <solnik2001@gmail.com>
2021-06-17 10:42:03 +0500

    Add autoconfigurate function

--- litecms/controllers/Admin.php
+++ litecms/controllers/Admin.php
@@ -2,8 +2,7 @@
 
 namespace Litecms\Controllers;
 
-use Litecms\Core\{Application, Controller, Connection, Request, View, Route};
-use Litecms\User\User;
+use Litecms\Core\{Application, Controller, Connection, Request, View};
 use const Litecms\Config\Models;
 
 class Admin extends Controller
@@ -51,6 +50,67 @@ class Admin extends Controller
 
     public static function configurate(Request $request)
     {
+        if ($request->method === 'POST' and !empty($request->post)) {
+            $settingsText = "<?php
+
+namespace Litecms\Config;
+
+// Warning! Disable on production
+const Debug = true;
+const Name = \"{$request->post['application_name']}\";
+const Timezone = \"UTF\";
+
+// Path to folders from document's root
+const Dirs = [
+    'models' => 'models',
+    'root' => 'litecms',
+    'static' => 'static',
+    'uploads' => 'uploads',
+
+    'templates' => [
+        'templates',
+    ],
+];
+
+// List of the installed applications.
+// Add your models into the END of the list
+const Models = [
+    'Litecms\User\User',
+    'Litecms\User\Group',
+];
+
+// Warning! Do not share your connection data to strangers!
+const Connection = [
+    'host' => '{$request->post['connection_host']}',
+    'username' => '{$request->post['connection_user']}',
+    'password' => '{$request->post['connection_password']}',
+    'database' => '{$request->post['connection_database']}',
+    'table_prefix' => '{$request->post['connection_prefix']}',
+];
+
+const SessionTime = 5;";
+            // Create settings file 
+            file_put_contents($_SERVER['DOCUMENT_ROOT']."/litecms/Settings.php", $settingsText);
+            message("success", "Файл настроек успешно создан", "");
+            sleep(10);
+
+            // Create database
+            Application::initializeDB();
+            message("success", "База данных успешно создана");
+            sleep(10);
+
+            // Create superuser
+            $admin = new \Litecms\User\User;
+            $admin->signup(
+                $request->post['admin_username'],
+                "{$request->post['admin_first_name']} {$request->post['admin_last_name']}",
+                $request->post['admin_email'],
+                $request->post['admin_password'],
+            );
+
+            message("success", "Всё готово для работы");
+            redirect("home", $request);
+        }
         return View::extend($request, "markups/base.php", "admin/configurate.php", ['title' => 'Первоначальная настройка системы']);
     }
 
