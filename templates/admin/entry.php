7cd44d0fcf5cdfaa902dc25ee1a228ccf6d5a93c
SolovovChann <solnik2001@gmail.com>
2021-07-04 14:00:11 +0500

    Add admin panel's templates

--- templates/admin/entry.php
+++ templates/admin/entry.php
@@ -1,11 +1,36 @@
 <a href="<?= url(Litecms\Controllers\Admin::class, "table", [$entry::$table]) ?>" class="btn btn-primary">&larr; Назад к списку записей</a>
 
 <form action="" class="form" method="POST">
-  <? foreach ($entry as $key => $value): ?>
+  <? foreach ($fields as $key => $value): ?>
   <div class="form__group">
     <label for="<?= $key ?>" class="form__label"><?= $key ?></label>
-    <input type="text" name="<?= $key ?>" value="<?= $value ?>" id="<?= $key ?>" class="form__input">
-  </div>
+
+    <? if ($value->mmetype === 'select'): ?>
+      <select name="<?= $key ?>" id="<?= $key ?>" class="form__input">
+        <option value="">Select <?= $key ?></option>
+        <? foreach ($value->reference::all() as $entry): ?>
+          <option value="<?= $entry->id ?>"><?= $entry ?></option>
+        <? endforeach; ?>
+      </select>
+
+    <? elseif ($value->mmetype === 'textarea'): ?>
+      <textarea name="<?= $key ?>" id="<?= $key ?>" class="form__input"><?= $entry->$key ?></textarea>
+    <? else: ?>
+    
+      <input
+        class="form__input"
+        id="<?= $key ?>"
+        <? if (isset($value->max)): ?>
+  maxlength="<?= $value->max ?>"
+        <? endif; ?>
+        name="<?= $key ?>"
+        type="<?= $value->mmetype ?>"
+        <? if (isset($entry->$key)): ?>
+  value="<?= $entry->$key ?>"
+        <? endif; ?>
+      >
+    </div>
+  <? endif; ?>
   <? endforeach; ?>
 
   <div class="form__submit-container">
--- templates/admin/index.php
+++ templates/admin/index.php
@@ -1,8 +1,8 @@
 <h2>Создать базу данных</h2>
-<a href="/admin/createdb" class="btn btn-primary">Создать базу данных</a>
+<a href="<?= url(\Litecms\Controllers\Admin::class, "createDB") ?>" class="btn btn-primary">Создать базу данных</a>
 
 <h2>Зарегистрированные модели</h2>
-<a href="<?= url(\Litecms\Controllers\Admin::class, "newmodel") ?>" class="btn btn-primary">&plus; Новая модель</a>
+<a href="<?= url(\Litecms\Controllers\Admin::class, "createModel") ?>" class="btn btn-primary">&plus; Новая модель</a>
 <ol>
 	<? foreach($models as $model): ?>
 		<li>
@@ -14,14 +14,14 @@
 <h2>Разегистрированные страницы</h2>
 <table class="table">
   <tr>
-    <th>Адрес</th>
+    <th>URL адрес</th>
     <th>Функция-обработчик</th>
     <th>Доступные методы</th>
   </tr>
   <tr>
     <? foreach($routes as $route): ?>
       <tr>
-        <td><?= $route['url'] ?></td>
+        <td><?= $route['url'] === '' ? '<Основная страница>' : $route['url']  ?></td>
         <td><?= gettype ($route['view']) === 'object' ? "Функция callback" : implode('::', $route['view']) ?></td>
         <td><?= implode(", ", $route['methods']) ?></td>
       </tr>
